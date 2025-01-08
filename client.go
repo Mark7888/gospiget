5a8f7ff5f3615234000ac25880dee9ef3fb51e86
Mark7888 <l.mark7888@gmail.com>
2025-01-08 17:13:42 +0100

    use random user agents

--- client.go
+++ client.go
@@ -7,10 +7,23 @@ import (
 	"os"
 	"time"
 
+	"math/rand"
+
 	"github.com/go-resty/resty/v2"
 )
 
 const baseURL = "https://api.spiget.org/v2"
+
+var userAgents = []string{
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15",
+}
+
+func getRandomUserAgent() string {
+	return userAgents[rand.Intn(len(userAgents))]
+}
 
 type Client struct {
 	restyClient *resty.Client
@@ -20,6 +33,7 @@ func NewClient() *Client {
 	client := resty.New()
 	client.SetBaseURL(baseURL)
 	client.SetTimeout(10 * time.Second)
+	client.SetHeader("User-Agent", getRandomUserAgent())
 	return &Client{restyClient: client}
 }
 
